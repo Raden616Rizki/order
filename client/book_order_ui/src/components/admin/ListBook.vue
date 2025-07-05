36ea707875b726640ec55436055b4f5828efdc61
rizkiraden <rizkiraden616@gmail.com>
2025-07-05 20:41:25 +0700

    Menambahkan halaman list buku

--- client/book_order_ui/src/components/admin/ListBook.vue
+++ client/book_order_ui/src/components/admin/ListBook.vue
@@ -1,6 +1,54 @@
 <template lang="">
-    <div>
-        ListBook
+    <div class="row mt-5">
+        <div class="col-md-10 m-auto">
+            <div class="card card-body text-center">
+                <h2>List of Book for Sale</h2>
+                <table class="table table-striped mt-4">
+                    <thead>
+                        <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>100</td>
+                            <td>1000</td>
+                            <td>
+                                <a href="javascript:void(0)" class="btn btn-sm btn-primary">Update</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>100</td>
+                            <td>1000</td>
+                            <td>
+                                <a href="javascript:void(0)" class="btn btn-sm btn-primary">Update</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>100</td>
+                            <td>1000</td>
+                            <td>
+                                <a href="javascript:void(0)" class="btn btn-sm btn-primary">Update</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
 <script>
